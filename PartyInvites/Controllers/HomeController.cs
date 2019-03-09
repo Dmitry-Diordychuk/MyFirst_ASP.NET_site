7a9b536d7fbaa2fdfabf1b394c3d10e2a466517f
Diord <Diord@DESKTOP-6B09NPQ>
2019-03-10 01:01:00 +0300

    pg 54

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -12,8 +12,23 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            Random rand = new Random();
+            int number = rand.Next(1, 100);
+            ViewBag.Greating = number < 50 ? "Good Morning Sir!" : "Good Afternoon";
             return View("MyView");
-            
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //Что сделать: сохранить ответ от гостя
+            return View();
         }
     }
 }
